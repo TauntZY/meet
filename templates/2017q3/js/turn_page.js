cf10cd5603719e0fc327be5edcd44e3d6a434fee
wuzhiyi <wzycrtd@163.com>
2016-08-08 15:11:06 +0800

    邀请函翻页效果、菜单按钮、修改字体，其他页面的邮箱类型设置为text

--- templates/2017q3/js/turn_page.js
+++ templates/2017q3/js/turn_page.js
@@ -1,103 +1,14 @@
 $(function(){
 
+    //创建fullpage
+    $('#con_inv').fullpage({
+        anchors:['page1','page2','page3'],
+        continuousVertical: false,
+        scrollingSpeed:500
+    });
+
     //----禁止 微信下拉查看网址
     $('body').on('touchmove', function (event) {event.preventDefault();});
-
-
-    /*
-    *========================================
-    * 页面跳转   开始
-    * =======================================
-    * */
-    //##########################第一
-    document.getElementById('page_inv_1').addEventListener("touchstart", function (e) {
-        _tounchSY = e.touches[0].pageY;
-    });
-    document.getElementById('page_inv_1').addEventListener("touchend", function (e) {
-        _tounchEY = e.changedTouches[0].pageY;
-        if (_tounchSY - _tounchEY > 80) {
-            //下一页
-
-            $("#page_inv_1").css({"z-index":"1"});
-            $("#page_inv_3").css({"z-index":"1"});
-
-            $("#page_inv_2").css({display: "block","z-index":"999"});
-            $("#page_inv_2").animate({marginTop:"0px"},400,"swing",function(){
-                $("#page_inv_1").css({display: "none",marginTop:"150%","z-index":"1"});
-                $("#page_inv_3").css({display: "none",marginTop:"150%","z-index":"1"});
-            });
-        }
-        if (_tounchSY - _tounchEY < -80) {
-            //上一页
-
-        }
-    });
-    //##########################第二
-    document.getElementById('page_inv_2').addEventListener("touchstart", function (e) {
-        _tounchSY = e.touches[0].pageY;
-    });
-    document.getElementById('page_inv_2').addEventListener("touchend", function (e) {
-        _tounchEY = e.changedTouches[0].pageY;
-        if (_tounchSY - _tounchEY > 80) {
-           // 下一页
-            $("#page_inv_1").css({"z-index":"1"});
-            $("#page_inv_2").css({"z-index":"1"});
-
-            $("#page_inv_3").css({display: "block","z-index":"9"});
-            $("#page_inv_3").animate({marginTop:"0px"},400,function(){
-                $("#page_inv_1").css({display: "none",marginTop:"150%","z-index":"1"});
-                $("#page_inv_2").css({display: "none",marginTop:"150%","z-index":"1"});
-            });
-        }
-        if (_tounchSY - _tounchEY < -80) {
-            //上一页
-            $("#page_inv_3").css({"z-index":"1"});
-            $("#page_inv_2").css({"z-index":"1"});
-
-            $("#page_inv_1").css({display: "block","z-index":"9",marginTop:"-150%"});
-            $("#page_inv_1").animate({marginTop:"0px"},400,function(){
-                $("#page_inv_3").css({display: "none",marginTop:"150%","z-index":"1"});
-                $("#page_inv_2").css({display: "none",marginTop:"150%","z-index":"1"});
-            });
-        }
-    });
-    //##########################第三
-    document.getElementById('page_inv_3').addEventListener("touchstart", function (e) {
-        _tounchSY = e.touches[0].pageY;
-    }, false);
-    document.getElementById('page_inv_3').addEventListener("touchend", function (e) {
-        _tounchEY = e.changedTouches[0].pageY;
-        if (_tounchSY - _tounchEY > 80) {
-            //下一页
-            $("#page_inv_2").css({"z-index":"1"});
-            $("#page_inv_3").css({"z-index":"1"});
-
-            $("#page_inv_1").css({display: "block","z-index":"9"});
-            $("#page_inv_1").animate({marginTop:"0px"},400,function(){
-                $("#page_inv_2").css({display: "none",marginTop:"150%"});
-                $("#page_inv_3").css({display: "none",marginTop:"150%"});
-            });
-        }
-        if (_tounchSY - _tounchEY < -80) {
-            //上一页
-
-            $("#page_inv_1").css({"z-index":"1"});
-            $("#page_inv_3").css({"z-index":"1"});
-
-            $("#page_inv_2").css({display: "block","z-index":"9",marginTop:"-150%"});
-            $("#page_inv_2").animate({marginTop:"0"},400,function(){
-                $("#page_inv_1").css({display: "none",marginTop:"150%"});
-                $("#page_inv_3").css({display: "none",marginTop:"150%"});
-            });
-
-
-        }
-    });
-    /*
-     *========================================
-     * 页面跳转   开始
-     * =======================================
-     * */
 
     //转发会议信息
     $(".share").click(function(){
@@ -114,9 +25,19 @@ $(function(){
         $(this).css({display:"none"});
     });
 
-
-    //编辑会议
-    $(".edit").click(function(){
-        $(".scaleControl").setAttribute('content',"width=640,scale=0.7");
+    //在输入信息的时候隐藏下一页的指示（防止挡住输入框）
+    $("#page_inv_3 .input").focusin(function(){
+        $("#next").hide();
     });
+    $("#page_inv_3 .input").focusout(function(){
+        $("#next").show();
+    });
+
+    $(".share").click(function(){
+        $(".shareop").css({display:'block'});
+    });
+    $(".shareop").click(function(){
+        $(this).css({display:'none'});
+    });
+
 });
